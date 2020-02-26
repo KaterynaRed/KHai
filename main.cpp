a429ed67774de077abb09ba9e41e4446a677891d
KAte <redkinakate@gmail.com>
2020-02-26 12:50:34 +0200

    main2

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 int main(int argc, char *argv[])
 {
 QApplication app(argc, argv);
-QPushButton button("Hello World!");
+QPushButton button("Hello World 1!");
 button.resize(200,60);
 button.show();
 
